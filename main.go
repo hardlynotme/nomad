967f23198f4a50ce7e2b1b04549ee3d5978c9718
Dave Morris <comtay67@gmail.com>
2017-07-25 20:32:28 -0400

    connecting to mango

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/mgo.v2"
+)
 
 func main() {
-	fmt.Printf("Hello World!\n")
+
+	session, err := mgo.Dial(os.Getenv("MONGO_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = session.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s!\n", os.Getenv("MONGO_URL"))
 }
